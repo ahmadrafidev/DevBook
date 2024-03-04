6a754e74160bde676dd4b8957dff88956fcacd04
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-03-04 22:42:39 +0700

    feat: revamp the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
-function HomepageHeader() {
+function Homepage() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
@@ -34,9 +33,8 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <Homepage />
       </main>
     </Layout>
   );
